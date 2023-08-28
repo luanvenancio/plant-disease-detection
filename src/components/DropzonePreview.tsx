c35d30e2ef51723f1c1bdf6d930d3bbc27306f65
Luan <luan.venancio04@hotmail.com>
2023-08-28 00:15:26 -0300

    Add Dropzone Preview

---
+++ src/components/DropzonePreview.tsx
@@ -0,0 +1,20 @@
+interface DropzonePreviewProps {
+    previewImg: string;
+    files?: File | null;
+}
+
+export function DropzonePreview({ previewImg, files }: DropzonePreviewProps) {
+    return (
+        <>
+            {previewImg && files && (
+                <div className="border w-80 rounded-lg m-4 p-2 bg-white border-gray-300 flex flex-row">
+                    <img className="rounded-lg w-32" src={previewImg} width="200px" />
+                    <div className="flex flex-col px-4 justify-center w-3/5">
+                        <p className="text-ellipsis overflow-hidden font-semibold">{files.name}</p>
+                        <p>{files.size > 1048576 ? `${(files.size / 1048576).toFixed(2)} MB` : `${(files.size / 1024).toFixed(2)} KB`}</p>
+                    </div>
+                </div>
+            )}
+        </>
+    )
+}
