3d957f5f667466cb45aa519dc3ac406f6d7eb32c
Luan <luan.venancio04@hotmail.com>
2023-08-28 00:14:54 -0300

    Add UploadIcon

--- src/icons/UploadIcon.tsx
+++ src/icons/UploadIcon.tsx
@@ -6,7 +6,7 @@ export const UploadIcon = (props: IconProps) => {
       {...props}
       aria-hidden="true"
       fill="none"
-      stroke="currentColor"
+      stroke="#FFFFFF"
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
     >
