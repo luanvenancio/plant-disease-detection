eb2d1c2dc7d26ab2eedd05bc30cc97fbf8fbb9e8
Luan <luan.venancio04@hotmail.com>
2023-10-15 15:40:34 -0300

    feat: Add Result Card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,20 @@ import { ChangeEvent, useMemo, useState } from "react";
 import { Dropzone } from "@/components/Dropzone";
 import useSWR from "swr";
 import Modal from "@/components/Modal";
-import { Plants } from "./plants/columns";
 import DataTablePage from "./plants/page";
+import { Card, CardContent, CardDescription, CardHeader } from "@/components/ui/card";
+import { ResultCard } from "@/components/ResultCard";
 
 type imgProps = {
   handleImg: (previewImg: File) => void;
+}
+
+export type Plants = {
+  name: string
+  result: string
+  status: "Pending" | "Analysing" | "Success" | "Failed"
+  score: number
+  accuracy: "High" | "Medium" | "Low"
 }
 
 export default function Home() {
@@ -32,9 +41,9 @@ export default function Home() {
     <main className="w-screen h-screen">
       <div className="flex flex-col items-center justify-center h-screen w-screen">
         <Modal handleResult={handleResult} handleImg={handleImg} />
-        <img className="rounded-lg w-1/4" src={img} width="200px" />
+        <img className="rounded-lg w-1/4 my-6" src={img} width="200px" />
         {result &&
-          <DataTablePage {...result} />
+          <ResultCard {...result} />
         }
       </div>
     </main>
