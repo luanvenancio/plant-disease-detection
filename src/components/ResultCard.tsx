0047658f2264758881f0fefbffe45c3dd6797f61
Luan <luan.venancio04@hotmail.com>
2023-10-31 21:45:08 -0300

    refractor: update result card

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -1,27 +1,50 @@
+import { Plants } from "@/app/util/PlantType";
+import Image from "next/image";
 import { Card } from "./ui/card";
+import {
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "./ui/card"
+import { Separator } from "./ui/separator";
 
-type Plants = {
-    name: string
-    result: string
-    status: "Pending" | "Analysing" | "Success" | "Failed"
-    score: number
-    accuracy: "High" | "Medium" | "Low"
+type ResultProps = {
+    result: Plants;
+    img: string;
 }
 
-export function ResultCard({ name, result, accuracy }: Plants) {
-
+export function ResultCard({ result, img }: ResultProps) {
     return (
-        <Card className="w-[340px] flex items-center">
-            <div className="p-4 flex justify-between items-center w-full">
-                <div>
-                    <p className="text-sm font-medium">{name}</p>
-                    <p className="text-md font-semibold">{result}</p>
-                </div>
-                <div>
-                    <p className="text-sm font-medium">Reliability</p>
-                    <p className="text-sm">{accuracy}</p>
-                </div>
+        <Card className="w-[512px] border dark:border-zinc-700">
+            <CardHeader className="border-b dark:border-zinc-700">
+                <CardTitle>Result</CardTitle>
+            </CardHeader>
+            <div className="flex justify-center">
+                <Image
+                    alt="Plant Image preview"
+                    src={img}
+                    width={256}
+                    height={256}
+                    style={{
+                        borderRadius: '0.5rem',
+                        marginTop: '1.5rem',
+                        marginBottom: '1.5rem'
+                    }}
+                />
             </div>
-        </Card>
+            <CardContent className="border-t dark:border-zinc-700">
+                <div className="pt-4 flex justify-between items-center w-full">
+                    <div>
+                        <p className="text-sm font-medium">{result.name}</p>
+                        <p className="text-md font-semibold">{result.result}</p>
+                    </div>
+                    <div>
+                        <p className="text-sm font-medium">Reliability</p>
+                        <p className="text-sm">{result.accuracy}</p>
+                    </div>
+                </div>
+            </CardContent>
+        </Card >
     )
 }
