ab8bd6c750e18d6328bbe539ae363e5dac812c2c
Luan <luan.venancio04@hotmail.com>
2023-10-31 21:44:49 -0300

    fix: prevent hydration problem

--- src/components/ui/mode-toggle.tsx
+++ src/components/ui/mode-toggle.tsx
@@ -1,26 +1,32 @@
-"use client"
+"use client";
 
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
 import { useTheme } from "next-themes"
 import { Button } from "@/components/ui/button"
+import { useState, useEffect } from "react";
 
 export function ModeToggle() {
     const { theme, setTheme } = useTheme()
 
+    const [mounted, setMounted] = useState(false);
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) return <></>;
+
     return (
-        <>
+        <div>
             {theme === 'dark' && (
                 <Button variant="outline" size="icon" onClick={() => setTheme("light")}>
-                    <MoonIcon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                    <span className="sr-only">Toggle theme</span>
+                    <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100" />
                 </Button>
             )}
             {theme === 'light' && (
                 <Button variant="outline" size="icon" onClick={() => setTheme("dark")}>
-                    <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <span className="sr-only">Toggle theme</span>
+                    <MoonIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0" />
                 </Button>
             )}
-        </>
+        </div>
     )
 }
