1e6c1b212804b6a304bdf24c18ff22a2c9b4a437
Luan <luan.venancio04@hotmail.com>
2023-11-12 12:33:29 -0300

    fix: close misssing icon

--- src/components/InputFile.tsx
+++ src/components/InputFile.tsx
@@ -2,7 +2,6 @@
 
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { CloseIcon } from "@/icons/CloseIcon";
 import { FileIcon } from "@/icons/FileIcon";
 import { UploadIcon } from "@/icons/UploadIcon";
 import { useCallback, useState } from "react";
@@ -48,32 +47,6 @@ export function InputFile() {
   if (file) return null;
 
   return <InputImage dropzone={dropzone} />;
-
-  /*<div className="flex flex-col items-center justify-center pt-5 pb-6">
-                    <svg
-                        className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 20 16"
-                    >
-                        <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                        />
-                    </svg>
-                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                        <span className="font-semibold">Click to upload</span>{" "}
-                        or drag and drop
-                    </p>
-                    <p className="text-xs text-gray-500 dark:text-gray-400">
-                        SVG, PNG, JPG or GIF (MAX. 800x400px)
-                    </p>
-                </div>
-                <Input id="picture" type="file" className="hidden" />*/
 }
 
 const InputImage = ({ dropzone }: InputProps) => {
