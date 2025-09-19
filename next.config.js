d25ad829ccd39440bcd390a033e09722d75e1e2b
bgvshiva2004 <gnanavenkatashivasshs@gmail.com>
2025-09-19 16:18:08 +0530

    PWA conversion fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,43 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const withPWA = require("next-pwa")({
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    register: true,
+    skipWaiting: true,
+});
+
+const nextConfig = {
+    distDir: "build",
+    reactStrictMode: true,
+    swcMinify: true,
+    compiler:{
+        removeConsole: process.env.NODE_ENV !== "development",
+    },
+    async headers() {
+        return [
+            {
+                source: "/manifest.json",
+                headers: [
+                    { key: "Content-Type", value: "application/manifest+json" },
+                    { key: "Cache-Control", value: "public, max-age=0, must-revalidate" },
+                ],
+            },
+            {
+                source: "/sw.js",
+                headers: [
+                    { key: "Service-Worker-Allowed", value: "/" },
+                    { key: "Cache-Control", value: "no-cache" },
+                ],
+            },
+            {
+                source: "/workbox-:hash.js",
+                headers: [
+                    { key: "Service-Worker-Allowed", value: "/" },
+                ],
+            },
+        ];
+    },
+}
+
+module.exports = withPWA(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Plant Disease Detection",
   description: "Plant Disease Detection",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["next.js","next14","next15","pwa","next-pwa"],
 };
 
 export const viewport: Viewport = {
@@ -26,6 +29,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+    <head>
+        <meta name="theme-color" content="#80b251" />
+        <link rel="manifest" href="/manifest.json"/>
+        <link rel="icon" href="/agriscan-512.png"/>
+        <link rel="apple-touch-icon" href="/agriscan-512.png"/>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="AgriScan" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <link rel="apple-touch-startup-image" href="/agriscan-512.png" />
+
+      </head>
       <body
         className={cn("bg-background font-sans antialiased", inter.className)}
       >
